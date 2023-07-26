b2a7468cce799ad7323ff318f158234b47226d1d
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-07-26 18:17:43 +0300

    Tailwind React project started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import styles from "./index.css"
+
+function App() {
+  return (
+    <div className="">
+    <h1 className=' text-red-500 '>This is a test</h1>
+    </div>
+  );
+}
+
+export default App;
