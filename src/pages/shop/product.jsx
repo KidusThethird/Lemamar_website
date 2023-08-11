4953e07723db07d1e519d74fb3abab74080ec7f6
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-08-11 12:57:10 +0300

    custom add to cart button made

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -6,10 +6,10 @@ export const Product = (props) => {
   return (
     
 
-    <div className='hover:scale-110  transition-all ease-in-out duration-200'>
+    <div className='hover:scale-110 group transition-all ease-in-out duration-200'>
          <div className='bg-fuchsia-800 w-fit px-5 text-white rounded-t-full'>{price} Birr</div>
-    <img src={productImage} alt="" className='border border-solid border-purple-600'/>
-   
+    <img src={productImage} alt="" className='border border-solid border-purple-600' />
+   <div className='absolute top-6 right-0 opacity-50 hover:opacity-100 px-2 py-1 text-sm text-white rounded-bl-lg cursor-pointer  bg-fuchsia-800 hidden group-hover:block duration-300 -pt-8'> Add to Cart</div>
     <div className=' bg-fuchsia-800  rounded-b-3xl hover:bg-purple-950 flex text-white justify-center' >
         
     <p className='py-2 px-1'>{productName}</p>
--- src/products.js
+++ src/products.js
@@ -14,5 +14,29 @@ export const PRODUCTS = [
         price: 650,
         productImage: productImage.p06
     },
+    {
+        id:3,
+        productName: 'MyProduct3',
+        price: 650,
+        productImage: productImage.p07
+    },
+    {
+        id:4,
+        productName: 'MyProduct4',
+        price: 650,
+        productImage: productImage.p08
+    },
+    {
+        id:5,
+        productName: 'MyProduct5',
+        price: 650,
+        productImage: productImage.p09
+    },
+    {
+        id:6,
+        productName: 'MyProduct6',
+        price: 650,
+        productImage: productImage.p10
+    },
 
 ];
