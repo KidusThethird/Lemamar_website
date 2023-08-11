81d0a349a8369439ef256f69a385c2f97eae9526
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-08-11 12:09:13 +0300

    price tag added

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -6,9 +6,12 @@ export const Product = (props) => {
   return (
     
 
-    <div className='product-holder'>
-    <img src={productImage} alt="" className='product-image-holder'/>
-    <div className=' product-items' >
+    <div className='hover:scale-110  transition-all ease-in-out duration-200'>
+         <div className='bg-fuchsia-800 w-fit px-5 text-white rounded-t-full'>{price} Birr</div>
+    <img src={productImage} alt="" className='border border-solid border-purple-600'/>
+   
+    <div className=' bg-fuchsia-800  rounded-b-3xl hover:bg-purple-950 flex text-white justify-center' >
+        
     <p className='py-2 px-1'>{productName}</p>
     </div>
 </div>
