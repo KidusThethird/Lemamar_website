b8572a80f247e6a430b87dd54e417cda6134949a
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-08-12 19:39:17 +0300

    category filter made

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import products from '../../productImage';
 import {Cover} from '../../components/cover';
 import { PRODUCTS } from '../../products';
 import { Product} from './product';
 
 export const Shop = () => {
+  
+
+  const [chosenCategory , setChosenCategory] = useState('all')
+
+  const skin=()=>{
+    
+    setChosenCategory('skin');
+    
+  }
+  const body=()=>{
+    
+    setChosenCategory('body');
+    
+  }
+  const tool=()=>{
+
+    setChosenCategory('tools');
+    
+  }
+  const all=()=>{
+ 
+    setChosenCategory('all');
+    
+  }
+
+
   return (
     <div className=''>
             <Cover />
 
-        <div className='flex justify-center'>
-            <h1 className='p-5  text-4xl'>Shop</h1>
+        <div className='flex  justify-center'>
+          <div>
+            <h1 className='p-5  text-4xl text-center'>Shop</h1>
+
+            <div className='space-x-7 mx-auto py-9'>
+              <button onClick={()=>all()} className='bg-purple-700 hover:scale-110 hover:shadow-2xl text-white px-4 rounded'>All</button>
+              <button onClick={()=>skin()} className='bg-purple-700 hover:scale-110 hover:shadow-2xl text-white px-4 rounded'>Skin Products</button>
+              <button onClick={()=>body()} className='bg-purple-700 hover:scale-110 hover:shadow-2xl text-white px-4 rounded'>Body Products</button>
+              <button onClick={()=>tool()} className='bg-purple-700 hover:scale-110 hover:shadow-2xl text-white px-4 rounded'>Tools</button>
+            </div>
+            </div>
         </div>
 
         
@@ -18,7 +53,10 @@ export const Shop = () => {
             <div className='grid grid-cols-5 p-6  gap-x-8 gap-y-4'>
                 
                     {PRODUCTS.map((product)=>(
-                        <Product data={product}/>
+                      product.category==chosenCategory?<Product data={product}/>: <>
+                       {chosenCategory=='all' && <Product data={product}/> }
+                      </>
+                        
                        
                     ))}
             </div>
--- src/products.js
+++ src/products.js
@@ -6,37 +6,43 @@ export const PRODUCTS = [
         id:1,
         productName: 'MyProduct',
         price: 850,
-        productImage: productImage.p05
+        productImage: productImage.p05,
+        category: 'tools'
     },
     {
         id:2,
         productName: 'MyProduct2',
         price: 650,
-        productImage: productImage.p06
+        productImage: productImage.p06,
+        category: 'skin'
     },
     {
         id:3,
         productName: 'MyProduct3',
         price: 650,
-        productImage: productImage.p07
+        productImage: productImage.p07,
+        category: 'tools'
     },
     {
         id:4,
         productName: 'MyProduct4',
         price: 650,
-        productImage: productImage.p08
+        productImage: productImage.p08,
+        category: 'body'
     },
     {
         id:5,
         productName: 'MyProduct5',
         price: 650,
-        productImage: productImage.p09
+        productImage: productImage.p09,
+        category: 'tools'
     },
     {
         id:6,
         productName: 'MyProduct6',
         price: 650,
-        productImage: productImage.p10
+        productImage: productImage.p10,
+        category: 'body'
     },
 
 ];
