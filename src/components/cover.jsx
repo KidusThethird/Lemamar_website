a59073de0ed8c2f1e898bdfc9f2db183e7ce5aa0
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-08-13 10:46:09 +0300

    cover slider done

--- src/components/cover.jsx
+++ src/components/cover.jsx
@@ -1,15 +1,61 @@
 import React from 'react';
 
-import CoveImage from '../assets/cover 2.jpg';
+import CoverImage1 from '../assets/cover-1.jpg';
+import CoverImage2 from '../assets/cover-2.png';
+import CoverImage3 from '../assets/cover-3.png';
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
 
 export function Cover() {
+
+const [cover, setCover] = useState(CoverImage1);
+
+const coverHolder = [CoverImage3, CoverImage2, CoverImage1];
+let index = 0;
+
+
+useEffect(() => {
+ 
+  const interval = setInterval(() => {
+  
+    if(index==0){
+      index=1;
+      setCover(coverHolder[1]);
+     
+    }
+    else if(index==1){
+      index=2;
+      setCover(coverHolder[2]);
+     
+    }
+    else if(index==2){
+      index=0;
+      setCover(coverHolder[0]);
+     
+    }
+  }, 5000);
+
+ 
+  return () => {
+    clearInterval(interval);
+  };
+}, []); 
+
+
+
   return (
     <div>
           {/* COver image Holder start */}
 
-<div className='sticky top-0'>
-  <img src={CoveImage} alt="" />
-</div>
+<motion.div  className='sticky top-0'
+            key={cover}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }} >
+  <img className='shadow-2xl' src={cover} alt="" />
+</motion.div>
 
   {/* Cover image Holder End */}
     
--- src/components/grid-items-ad.jsx
+++ src/components/grid-items-ad.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import products from '../productImage';
 import bg1 from '../assets/bg1.jpg';
+import bg2 from '../assets/bg2.jpg';
 
 export const GridItemsAd = () => {
   return (
@@ -12,32 +13,32 @@ export const GridItemsAd = () => {
 
 
 <div className='relative'>
-  <img src={bg1} className=' w-full' alt="" />
+  <img src={bg2} className=' w-full' alt="" />
 
   <div className='absolute w-1/2 h-full flex  py-10  right-0 top-0'>
      <div className='grid grid-cols-2 gap-x-10   items-center '>
           <div className='group relative '>
             <img className='one_of_four_images ' src={products.p14} alt="" />
               <div className='animated-cover'>
-                <h1 className=' text-white text-4xl font-bold uppercase text-center my-auto mx-auto'>Skin Care</h1>
+                <h1 className=' text-white text-3xl font-bold uppercase text-center my-auto mx-auto'>Skin Care</h1>
               </div>
           </div>
           <div className='group relative '>
             <img className='one_of_four_images ' src={products.p13} alt="" />
               <div className='animated-cover'>
-                <h1 className=' text-white text-4xl font-bold uppercase text-center my-auto mx-auto'>Body</h1>
+                <h1 className=' text-white text-3xl font-bold uppercase text-center my-auto mx-auto'>Body</h1>
               </div>
           </div>
           <div className='group relative '>
             <img className='one_of_four_images ' src={products.p03} alt="" />
               <div className='animated-cover'>
-                <h1 className=' text-white text-4xl font-bold uppercase text-center my-auto mx-auto'>Attractive</h1>
+                <h1 className=' text-white text-3xl font-bold uppercase text-center my-auto mx-auto'>Attractive</h1>
               </div>
           </div>
           <div className='group relative '>
             <img className='one_of_four_images ' src={products.p04} alt="" />
               <div className='animated-cover'>
-                <h1 className=' text-white text-4xl font-bold uppercase text-center my-auto mx-auto'>Bath</h1>
+                <h1 className=' text-white text-3xl font-bold uppercase text-center my-auto mx-auto'>Bath</h1>
               </div>
           </div>
           
